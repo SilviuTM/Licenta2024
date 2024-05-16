9f252789e9b5fc3e8d80432795e5e50ab69d56e4
gandolh <29356778+gandolh@users.noreply.github.com>
2024-05-16 21:38:36 +0300

    added whiteboard

---
+++ simulation/data/Whiteboard.js
@@ -0,0 +1,61 @@
+class Whiteboard {
+    constructor() {
+        this.currentShape = 'none';
+    }
+
+    draw() {
+        // Drawing code goes here
+    }
+
+    setShape(shape, el) {
+        const buttons = document.querySelectorAll('.ui-container-btn');
+        buttons.forEach((button) => {
+          button.classList.remove('active');
+        });
+        el.classList.add('active');
+        this.currentShape = shape;
+      }
+
+      placeShape() {
+        if (this.currentShape === 'none')
+          return;
+      
+        const sceneEl = document.querySelector('a-scene');
+        const cursorEl = document.querySelector('#cursor');
+        const intersection = cursorEl.components.raycaster.getIntersection(document.querySelector('.clickable'));
+      
+        if (intersection && this.currentShape !== 'delete') {
+          let shapeEl;
+          if (this.currentShape === 'box') {
+            shapeEl = document.createElement('a-box');
+            shapeEl.setAttribute('width', 1);
+            shapeEl.setAttribute('height', 1);
+            shapeEl.setAttribute('depth', 1);
+          } else if (this.currentShape === 'sphere') {
+            shapeEl = document.createElement('a-sphere');
+            shapeEl.setAttribute('radius', 0.5);
+          } else if (this.currentShape === 'cylinder') {
+            shapeEl = document.createElement('a-cylinder');
+            shapeEl.setAttribute('radius', 0.5);
+            shapeEl.setAttribute('height', 1);
+          }
+      
+          shapeEl.setAttribute('position', intersection.point);
+          shapeEl.setAttribute('color', '#4CC3D9');
+          shapeEl.setAttribute('class', 'deletable');
+          shapeEl.setAttribute('dynamic-body', '');
+          shapeEl.addEventListener('click', function () {
+            if (this.currentShape === 'delete') {
+              sceneEl.removeChild(shapeEl);
+            }
+          });
+      
+          sceneEl.appendChild(shapeEl);
+        } else if (intersection && this.currentShape === 'delete') {
+          const deletableIntersection = cursorEl.components.raycaster.getIntersection(document.querySelector('.deletable'));
+          if (deletableIntersection) {
+            sceneEl.removeChild(deletableIntersection.object.el);
+          }
+        }
+      }
+}
