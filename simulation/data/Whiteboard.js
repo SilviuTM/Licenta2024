25e99e9e79375bfb6260582b91470d02969f5901
gandolh <29356778+gandolh@users.noreply.github.com>
2024-05-16 21:45:40 +0300

    class

--- simulation/data/Whiteboard.js
+++ simulation/data/Whiteboard.js
@@ -31,13 +31,16 @@ class Whiteboard {
             shapeEl.setAttribute('width', 1);
             shapeEl.setAttribute('height', 1);
             shapeEl.setAttribute('depth', 1);
+            intersection.point.z += 0.5;
           } else if (this.currentShape === 'sphere') {
             shapeEl = document.createElement('a-sphere');
             shapeEl.setAttribute('radius', 0.5);
+            intersection.point.z += 0.5;
           } else if (this.currentShape === 'cylinder') {
             shapeEl = document.createElement('a-cylinder');
             shapeEl.setAttribute('radius', 0.5);
             shapeEl.setAttribute('height', 1);
+            intersection.point.z += 0.5;
           }
       
           shapeEl.setAttribute('position', intersection.point);
--- simulation/simulation.js
+++ simulation/simulation.js
@@ -1,63 +1,7 @@
-let currentShape = 'none'; // Default shape
-
-function setShape(shape, el) {
-  const buttons = document.querySelectorAll('.ui-container-btn');
-  buttons.forEach((button) => {
-    button.classList.remove('active');
-  });
-  el.classList.add('active');
-  currentShape = shape;
-}
-
-// Function to create and place a shape at the cursor's intersection point
-function placeShape() {
-  if (currentShape === 'none')
-    return;
-
-  const sceneEl = document.querySelector('a-scene');
-  const cursorEl = document.querySelector('#cursor');
-  const intersection = cursorEl.components.raycaster.getIntersection(document.querySelector('.clickable'));
-
-  if (intersection && currentShape !== 'delete') {
-    let shapeEl;
-    if (currentShape === 'box') {
-      shapeEl = document.createElement('a-box');
-      shapeEl.setAttribute('width', 1);
-      shapeEl.setAttribute('height', 1);
-      shapeEl.setAttribute('depth', 1);
-      intersection.point.z += 0.5;
-    } else if (currentShape === 'sphere') {
-      shapeEl = document.createElement('a-sphere');
-      shapeEl.setAttribute('radius', 0.5);
-      intersection.point.z += 0.5;
-    } else if (currentShape === 'cylinder') {
-      shapeEl = document.createElement('a-cylinder');
-      shapeEl.setAttribute('radius', 0.5);
-      shapeEl.setAttribute('height', 1);
-      intersection.point.z += 0.5;
-    }
-
-    shapeEl.setAttribute('position', intersection.point);
-    shapeEl.setAttribute('color', '#4CC3D9');
-    shapeEl.setAttribute('class', 'deletable');
-    shapeEl.setAttribute('dynamic-body', '');
-    shapeEl.addEventListener('click', function () {
-      if (currentShape === 'delete') {
-        sceneEl.removeChild(shapeEl);
-      }
-    });
-
-    sceneEl.appendChild(shapeEl);
-  } else if (intersection && currentShape === 'delete') {
-    const deletableIntersection = cursorEl.components.raycaster.getIntersection(document.querySelector('.deletable'));
-    if (deletableIntersection) {
-      sceneEl.removeChild(deletableIntersection.object.el);
-    }
-  }
-}
+const whiteboard = new Whiteboard();  
 
 document.querySelector('a-scene').addEventListener('contextmenu', function (e) {
   e.preventDefault();
   e.stopPropagation();
-  placeShape();
+  whiteboard.placeShape();
 });
